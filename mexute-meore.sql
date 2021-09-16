58756f56248631961e4462728c6eec4aa540918a
DavitKristesiashvili <85955367+DavitKristesiashvili@users.noreply.github.com>
2021-09-16 18:38:41 +0400

    Add files via upload

---
+++ mexute-meore.sql
@@ -0,0 +1,11 @@
+﻿
+---pirveli varianti 
+select [dbo].[Teacher].* from [dbo].[Teacher] 
+join [dbo].[TeacherPupil] on [dbo].[TeacherPupil].TeacherID = [dbo].[Teacher].TeacherID 
+join [dbo].[Pupil] on [dbo].[Pupil].PupilID = [dbo].[TeacherPupil].PupilID 
+where [dbo].[Pupil].FirstName = N'გიორგი' 
+
+---meore varianti 
+select [dbo].[Teacher].* from [dbo].[Teacher] where [dbo].[Teacher].TeacherID in 
+(select [dbo].[TeacherPupil].TeacherID from [dbo].[TeacherPupil] where [dbo].[TeacherPupil].PupilID in 
+(select [dbo].[Pupil].PupilID from [dbo].[Pupil] where [dbo].[Pupil].FirstName = N'გიორგი')) 
